bfaa8f4f2afc5ba598b3e639f0de2cda92502ccd
abedsipayung <53556147+sipayungabed@users.noreply.github.com>
2023-09-03 21:20:39 +0700

    AFS - Add Navbar

---
+++ constant/index.ts
@@ -0,0 +1,7 @@
+export const NavLinks = [
+    { href: '/', key: 'Home', text: 'Home' },
+    { href: '/', key: 'About', text: 'About' },
+    { href: '/', key: 'Experience', text: 'Experience' },
+    { href: '/', key: 'Project', text: 'Project' },
+    { href: '/', key: 'Contact', text: 'Contact' }
+  ];
